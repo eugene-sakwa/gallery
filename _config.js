e4bc4d22533a29ffb18cac897f3b1246e0697dd0
eugene-sakwa <the.ardent.arian@gmail.com>
2024-04-15 10:15:00 +0200

    Made changes to test the webhook

--- _config.js
+++ _config.js
@@ -1,6 +1,7 @@
 var config = {}
 
 // Update to have your correct username and password
+//Still not able to connect to the db
 config.mongoURI = {
     production: 'mongodb+srv://<eugene_don>:<eugene_don>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://<eugene_don>:<eugene_don>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
