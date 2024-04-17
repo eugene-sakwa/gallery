d863e6c7a59539c12fb20b1d8a34946793628b3a
eugene-sakwa <the.ardent.arian@gmail.com>
2024-04-17 09:05:40 +0200

    Modified _config.js file to add production and deployment uri

--- _config.js
+++ _config.js
@@ -3,9 +3,9 @@ var config = {}
 // Update to have your correct username and password
 //Still not able to connect to the db
 config.mongoURI = {
-    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net'
-    //production: 'mongodb+srv://<eugene_don>:<eugene_don>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    //development: 'mongodb+srv://<eugene_don>:<eugene_don>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    //test: 'mongodb+srv://<eugene_don>:<eugene_don>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    //test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net'
+    production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net',
+    development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net',
+    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net',
 }
 module.exports = config;
