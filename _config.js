79692f8ab65a294bd589e4b64231a7df04afe7ca
eugene-sakwa <the.ardent.arian@gmail.com>
2024-04-19 14:51:39 +0200

    Fixed typo on _config.js

--- _config.js
+++ _config.js
@@ -1,10 +1,10 @@
 var config = {}
 
-// Update to have your correct username and password
-//Still not able to connect to the db
 config.mongoURI = {
-    development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=maority',
-    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=maority',
-    production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=maority',
+    // test: 'mongodb+srv://eugene_don:<password>@ip-one.gjop4bg.mongodb.net/',
+    development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=majority',
+    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=majority',
+    production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=majority',
+
 }
 module.exports = config;
