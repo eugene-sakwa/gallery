cf7209b56a6048669b932f717095ea850a1e94a1
eugene-sakwa <the.ardent.arian@gmail.com>
2024-04-19 14:41:44 +0200

    Modify _config.js to include the route of the images

--- _config.js
+++ _config.js
@@ -3,9 +3,8 @@ var config = {}
 // Update to have your correct username and password
 //Still not able to connect to the db
 config.mongoURI = {
-    //test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net'
-    production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net',
-    development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net',
-    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net',
+    development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=maority',
+    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=maority',
+    production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retyWrites=true&w=maority',
 }
 module.exports = config;
