042eb53546487f31ac3241abc0ce1a4b3ec983d7
eugene-sakwa <the.ardent.arian@gmail.com>
2024-04-19 21:26:26 +0200

    Fixed typo on _config.js

--- _config.js
+++ _config.js
@@ -1,15 +1,12 @@
 var config = {}
 
 config.mongoURI = {
-    // test: 'mongodb+srv://eugene_don:<password>@ip-one.gjop4bg.mongodb.net/',
-    development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retryWrites=true&w=majority',
-    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retryWrites=true&w=majority',
-    production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/?retryWrites=true&w=majority&appName=IP-One',
+    //development: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retryWrites=true&w=majority',
+    test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/?retryWrites=true&w=majority&appName=IP-One',
+    //test: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retryWrites=true&w=majority',
+    production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/?retryWrites=true&w=majority&appName=IP-One'
+    //production: 'mongodb+srv://eugene_don:<eugene_don>@ip-one.gjop4bg.mongodb.net/darkroom?retryWrites=true&w=majority',
 
 }
 module.exports = config;
-
-
-// production: 'mongodb+srv://admin:CqSH70opjCd2cI3J@atlascluster.ijood98.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster',
-// development: 'mongodb+srv://admin:CqSH70opjCd2cI3J@atlascluster.ijood98.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster',
-// test: 'mongodb+srv://admin:CqSH70opjCd2cI3J@atlascluster.ijood98.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster',
